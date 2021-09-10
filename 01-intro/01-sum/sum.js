c8f032ab4a1713d023aaeaec11c75ed36fdbe442
Bespalov Vladyslav <bespalov.vladyslav@airslate.com>
2021-09-10 18:09:57 +0300

    Was added solution for 1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
+const isNumber = (value) => typeof value === 'number';
+
 function sum(a, b) {
-  /* ваш код */
+  if (isNumber(a) && isNumber(b)) {
+    return a + b;
+  }
+
+  throw new TypeError('All params of function must be numbers');
 }
 
 module.exports = sum;
